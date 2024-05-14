9afe737ee11b2afdf53d8160f5d1a5a55aa448ae
Hector Martin <hectormartindama@gmail.com>
2024-05-14 23:33:55 +0200

    fix: add support image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [
+    tailwind(),
+    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
+  ],
   markdown: {
     theme: "dracula",
     themes: {
