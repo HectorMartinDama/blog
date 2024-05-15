2250a8b2cace411bb638ba00918ef96040a5a7a9
Hector Martin <hectormartindama@gmail.com>
2024-05-15 21:10:51 +0200

    feat: add the cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
@@ -15,5 +17,5 @@ export default defineConfig({
     wrap: true,
   },
   output: "hybrid",
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
